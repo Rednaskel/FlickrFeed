2d83e3250cde3e7b993aa8ba13d5d16445229e60
Maciej Malak <maciej.malak@blstream.com>
2017-01-12 15:01:10 +0100

    Autogenerated Android project

---
+++ app/src/main/java/com/malak/yaim/FlickrFeed.java
@@ -0,0 +1,12 @@
+package com.malak.yaim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlickrFeed extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_flickr_feed);
+  }
+}
