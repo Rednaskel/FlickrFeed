d44942fefacf513b4fad5ec7c8ed7b4ee45b2d7a
Maciej Malak <maciej.malak@blstream.com>
2017-01-13 13:07:37 +0100

    Basic application DI graph with shared app context per app instance

--- app/src/main/java/com/malak/yaim/FlickrFeed.java
+++ app/src/main/java/com/malak/yaim/FlickrFeed.java
@@ -1,7 +1,7 @@
 package com.malak.yaim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class FlickrFeed extends AppCompatActivity {
 
