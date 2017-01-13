21edac6235784a6a9ea19f05b196570e6803d4ad
Maciej Malak <maciej.malak@blstream.com>
2017-01-13 13:56:47 +0100

    DI configuration for Activity level

---
+++ app/src/main/java/com/malak/yaim/di/modules/FlickrFeedModule.java
@@ -0,0 +1,24 @@
+package com.malak.yaim.di.modules;
+
+import com.malak.yaim.FlickrFeed;
+import com.malak.yaim.di.scopes.PerActivity;
+import com.malak.yaim.services.FlickrService;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class FlickrFeedModule {
+  private FlickrFeed mActivity;
+
+  public FlickrFeedModule(final FlickrFeed activity) {
+    mActivity = activity;
+  }
+
+  @Provides @PerActivity FlickrFeed providesFlickrActivity() {
+    return mActivity;
+  }
+
+  @Provides @PerActivity FlickrService providesFlickrService() {
+    return new FlickrService();
+  }
+}
