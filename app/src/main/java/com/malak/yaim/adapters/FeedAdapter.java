013fadaa81c3b6bbc4ae7e116a688b19b5fd22da
Maciej Malak <maciejmalak@gmail.com>
2017-01-14 17:26:00 +0100

    Injecting Adapter to View

--- app/src/main/java/com/malak/yaim/adapters/FeedAdapter.java
+++ app/src/main/java/com/malak/yaim/adapters/FeedAdapter.java
@@ -2,8 +2,19 @@ package com.malak.yaim.adapters;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
+import com.malak.yaim.model.Item;
+import java.util.List;
+import javax.inject.Inject;
 
 public class FeedAdapter extends RecyclerView.Adapter {
+  private List<Item> mImagesMetadata;
+
+  @Inject FeedAdapter() {}
+
+  public void setDataset(final List<Item> items) {
+    mImagesMetadata = items;
+  }
+
   @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
     return null;
   }
--- app/src/main/java/com/malak/yaim/view/FlickrActivity.java
+++ app/src/main/java/com/malak/yaim/view/FlickrActivity.java
@@ -3,11 +3,13 @@ package com.malak.yaim.view;
 import android.os.Bundle;
 import com.malak.yaim.BaseActivity;
 import com.malak.yaim.R;
+import com.malak.yaim.adapters.FeedAdapter;
 import com.malak.yaim.presentation.FeedPresenter;
 import javax.inject.Inject;
 
 public class FlickrActivity extends BaseActivity {
   @Inject FeedPresenter mPresenter;
+  @Inject FeedAdapter mAdapter;
 
   @Override public void initializeInjector() {
     getApplicationComponent().inject(this);
