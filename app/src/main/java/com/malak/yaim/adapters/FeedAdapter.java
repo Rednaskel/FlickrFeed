b3a26bc88cb3af7450ac03f8b975698515ebc01e
Maciej Malak <maciejmalak@gmail.com>
2017-01-14 18:41:17 +0100

    Applying ViewHolder patter. Creating layout for Flickr feed item

--- app/src/main/java/com/malak/yaim/adapters/FeedAdapter.java
+++ app/src/main/java/com/malak/yaim/adapters/FeedAdapter.java
@@ -1,12 +1,18 @@
 package com.malak.yaim.adapters;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import com.malak.yaim.R;
 import com.malak.yaim.model.Item;
 import java.util.List;
 import javax.inject.Inject;
 
-public class FeedAdapter extends RecyclerView.Adapter {
+public class FeedAdapter extends RecyclerView.Adapter<FeedAdapter.ViewHolder> {
   private List<Item> mImagesMetadata;
 
   @Inject FeedAdapter() {}
@@ -15,15 +21,29 @@ public class FeedAdapter extends RecyclerView.Adapter {
     mImagesMetadata = items;
   }
 
-  @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return null;
+  @Override public FeedAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    final View view = LayoutInflater
+        .from(parent.getContext())
+        .inflate(R.layout.feed_item, parent, false);
+
+    return new ViewHolder(view);
   }
 
-  @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-
+  @Override public void onBindViewHolder(FeedAdapter.ViewHolder holder, int position) {
+    /* TODO 1. add Picasso call */
+    /*TODO 2. bind metadata to text views */
   }
 
   @Override public int getItemCount() {
-    return 0;
+    return mImagesMetadata != null ? mImagesMetadata.size() : 0;
+  }
+
+  static class ViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.feed_item_image) ImageView mFeedItemImage;
+
+    ViewHolder(View view) {
+      super(view);
+      ButterKnife.bind(this, view);
+    }
   }
 }
