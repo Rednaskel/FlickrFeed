aca8d4e2f0da46c943fe7ad76894a16d98616177
Maciej Malak <maciejmalak@gmail.com>
2017-01-14 16:53:27 +0100

    Clean adapter without implementation

---
+++ app/src/main/java/com/malak/yaim/adapters/FeedAdapter.java
@@ -0,0 +1,18 @@
+package com.malak.yaim.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class FeedAdapter extends RecyclerView.Adapter {
+  @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    return null;
+  }
+
+  @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+  }
+
+  @Override public int getItemCount() {
+    return 0;
+  }
+}
